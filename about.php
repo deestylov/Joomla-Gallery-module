fa64efb4a039ff60b0fc0adf51fed893d9108af3
Roman Deestylov <83674622+deestylov@users.noreply.github.com>
2022-10-08 11:56:15 +0300

    master v 0.1

---
+++ about.php
@@ -0,0 +1,24 @@
+﻿<?php
+/**
+* About block Element
+* @package News Show Pro GK4
+* @Copyright (C) 2009-2011 Gavick.com
+* @ All rights reserved
+* @ Joomla! is Free Software
+* @ Released under GNU/GPL License : http://www.gnu.org/copyleft/gpl.html
+* @version $Revision: GK4 1.0 $
+**/
+defined('JPATH_BASE') or die;
+jimport('joomla.form.formfield');
+class JFormFieldAbout extends JFormField {
+	protected $type = 'About';
+	protected function getInput() {
+	return '
+    <style>#jform_params_about_us-lbl {display:none;}#jform_params_stylecarus {width:100%!important; height:230px!important;}</style>
+<div style="margin-bottom:5px;font-size: 14px;">Автор: Роман Дистайлов</div>
+<div style="
+    font-size: 19px;
+">Модуль позволяет выводит галерею на вашем сайте.</div>';
+	}
+}
+/* eof */
---
+++ mod_deestylov_gallery.php
@@ -0,0 +1,4 @@
+<?php defined('_JEXEC') or die;
+require JModuleHelper::getLayoutPath('mod_deestylov_gallery', $params->get('layout', 'default'));
+?>
+
